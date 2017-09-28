26bbefdc5b6648e6138d83ca4b74a49eb714d6ba
websivustot <websivustot@gmail.com>
2017-09-28 12:46:43 +0300

    new repository

---
+++ app/templates/layout.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Sanat</title>
+  <link rel="stylesheet" href="css/libs.min.css">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <div class="container">
+      <?=$content?>
+    </div>
+    <script src="js/libs.min.js" ></script>
+    <script src="js/common.js" ></script>
+</body>
+</html>
